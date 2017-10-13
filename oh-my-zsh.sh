14f013729070c459a38f2156aa92cec65291f700
Thomas Miller <thomas.miller@pgn.com>
2017-10-13 10:48:58 -0700

    Updated zcompdump to not clutter the  dir

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -56,7 +56,7 @@ fi
 
 # Save the location of the current completion dump file.
 if [ -z "$ZSH_COMPDUMP" ]; then
-  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.cache/zsh/zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 fi
 
 if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
